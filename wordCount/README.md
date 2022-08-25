7b5ce465fa461f73e87e8c7063371204fa750963
cpucode <923992029@qq.com>
2022-08-25 17:29:06 +0800

    文件创建RDD的默认分区数

--- wordCount/README.md
+++ wordCount/README.md
@@ -11,6 +11,7 @@
 - [x] [从集合创建RDD](src/main/java/com/cpucode/spark/create/rdd/CreateRddArray.scala)
 - [x] [集合创建RDD的默认分区数](src/main/java/com/cpucode/spark/partition/default/array/PartiDefaArray.scala)
 - [x] [集合创建RDD的指定分区数](src/main/java/com/cpucode/spark/partition/costomize/array/PartiCosArray.scala)
+- [x] [文件创建RDD的默认分区数](src/main/java/com/cpucode/spark/partition/default/file/ParDefFile.scala)
 
 ------------
 
--- wordCount/src/main/java/com/cpucode/spark/partition/default/file/ParDefFile.scala
+++ wordCount/src/main/java/com/cpucode/spark/partition/default/file/ParDefFile.scala
@@ -1,5 +1,7 @@
 package com.cpucode.spark.partition.default.file
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
  * @Author CpuCode
  * @Github https://github.com/CPU-Code
@@ -7,5 +9,15 @@ package com.cpucode.spark.partition.default.file
  * @Date 2022/8/24 17:27
  */
 object ParDefFile {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[*]").setAppName("ParDefFile")
+    val sc = new SparkContext(conf)
 
+    //默认分区的数量：默认取值为当前核数和2的最小值
+    val rdd = sc.textFile("wordCount/src/main/resources/word.txt")
+
+    rdd.saveAsTextFile("wordCount/output")
+
+    sc.stop()
+  }
 }
